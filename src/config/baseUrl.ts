4000c12fca6b492c9ec26161be7421c5137cffbf
Zakaria <zaknetsum@gmail.com>
2023-10-07 16:55:31 +0600

    make it better

--- src/config/baseUrl.ts
+++ src/config/baseUrl.ts
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'development') {
 	baseUrl = 'http://localhost:5005/api/v1';
 	console.log('Running in development mode');
 } else {
-	baseUrl = 'https://wfc-api.herokuapp.com/api/v1';
+	baseUrl = 'https://express-life-9388765cfa58.herokuapp.com/api/v1';
 	// Code specific to production mode
 	console.log('Running in production mode');
 }
