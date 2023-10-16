6c800cd03fcb49087c94c13f4e9b37d31346f747
Zakaria <zaknetsum@gmail.com>
2023-10-16 22:05:30 +0600

    make it better

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -206,7 +206,7 @@ const Register = () => {
 			country_code: code,
 			password: password,
 			confirm_password: confirmPassword,
-			referral: referralCode ? referralCode : '169667654409',
+			referral: referralCode ? referralCode : '169747226270',
 		};
 
 		registerUser(data);
