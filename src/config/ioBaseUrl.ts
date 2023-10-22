5d3d85bc1f72a152456d3623abf273644f7e8a9f
Zakaria <zaknetsum@gmail.com>
2023-10-22 09:07:37 +0600

    chnage api url

--- src/config/ioBaseUrl.ts
+++ src/config/ioBaseUrl.ts
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === 'development') {
 	// Code specific to development mode
 	ioBaseUrl = 'http://localhost:5005';
 } else {
-	ioBaseUrl = 'https://express-life-9388765cfa58.herokuapp.com';
+	ioBaseUrl = 'https://express-life-api-b43b2de765f9.herokuapp.com';
 	// Code specific to production mode
 }
 export default ioBaseUrl;
