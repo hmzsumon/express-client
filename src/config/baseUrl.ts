b6f701827bfe86cfb281dbd6b4af31e6a6fb87fc
Zakaria <zaknetsum@gmail.com>
2023-12-06 03:05:03 +0600

    make it better

--- src/config/baseUrl.ts
+++ src/config/baseUrl.ts
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'development') {
 	baseUrl = 'http://localhost:5005/api/v1';
 	console.log('Running in development mode');
 } else {
-	baseUrl = 'https://express-life-api-b43b2de765f9.herokuapp.com/api/v1';
+	baseUrl = 'https://expresslife-d28360a1553b.herokuapp.com/api/v1';
 	// Code specific to production mode
 	console.log('Running in production mode');
 }
--- src/config/ioBaseUrl.ts
+++ src/config/ioBaseUrl.ts
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === 'development') {
 	// Code specific to development mode
 	ioBaseUrl = 'http://localhost:5005';
 } else {
-	ioBaseUrl = 'https://express-life-api-b43b2de765f9.herokuapp.com';
+	ioBaseUrl = 'https://expresslife-d28360a1553b.herokuapp.com';
 	// Code specific to production mode
 }
 export default ioBaseUrl;
